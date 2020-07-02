c78538f8dc0dc12e952b56763f507d2d13f367d5
Navin <navin007.a@gmail.com>
2020-07-02 08:10:20 +0530

    feat: :sparkles: add next js and chakra ui

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/core";
+
+import theme from "../chakra";
+
+export default ({ Component, pageProps }) => (
+  <ChakraProvider theme={theme}>
+    <CSSReset />
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
