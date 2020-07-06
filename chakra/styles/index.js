32e68236af7cfe13624a4319aa194f71f3d05cb2
Navin <navin007.a@gmail.com>
2020-07-06 23:13:41 +0530

    perf: :zap: remove color mode and unused codes

--- chakra/styles/index.js
+++ chakra/styles/index.js
@@ -1,20 +1,18 @@
-import { mode } from '@chakra-ui/theme-tools';
-
 const styles = {
-  global: props => ({
-    fontFamily: 'body',
-    color: mode('gray.800', 'whiteAlpha.900')(props),
-    bg: mode('white', 'gray.800')(props),
-    lineHeight: 'base',
-    '*::placeholder': {
-      color: mode('gray.400', 'whiteAlpha.400')(props),
+  global: (props) => ({
+    fontFamily: "body",
+    color: "gray.800",
+    bg: "white",
+    lineHeight: "base",
+    "*::placeholder": {
+      color: "gray.400",
     },
-    '*, *::before, &::after': {
-      borderColor: mode('gray.200', 'whiteAlpha.300')(props),
-      wordWrap: 'break-word',
+    "*, *::before, &::after": {
+      borderColor: "gray.200",
+      wordWrap: "break-word",
     },
     fontFeatureSettings: `"pnum"`,
-    fontVariantNumeric: 'proportional-nums',
+    fontVariantNumeric: "proportional-nums",
   }),
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/system";
+import { ThemeProvider, GlobalStyle } from "@chakra-ui/system";
 import { CSSReset } from "@chakra-ui/css-reset";
 
 import theme from "../chakra";
 
 export default ({ Component, pageProps }) => (
-  <ChakraProvider theme={theme}>
+  <ThemeProvider theme={theme}>
     <CSSReset />
+    <GlobalStyle />
     <Component {...pageProps} />
-  </ChakraProvider>
+  </ThemeProvider>
 );
