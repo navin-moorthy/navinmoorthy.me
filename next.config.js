94e8f020348cbe36f0fb6db168db48b32bc25dde
Navin <navin007.a@gmail.com>
2020-07-07 11:28:03 +0530

    feat: :sparkles: add sitemap to the project

--- next.config.js
+++ next.config.js
@@ -6,4 +6,11 @@ module.exports = withBundleAnalyzer({
   experimental: {
     productionBrowserSourceMaps: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap")
+    }
+
+    return config
+  },
 })
