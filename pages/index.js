5947bb24baf2641c1695670ac3202e752db3fb52
Navin <navin007.a@gmail.com>
2020-07-12 20:01:11 +0530

    feat: :sparkles: add new single page index

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,28 @@
-import { Center, Heading, Text, Code, Link } from "@chakra-ui/layout"
-import { Logo } from "../components/logo"
+import { Center, Heading, Text, Flex } from "@chakra-ui/layout"
 
 export default function Home() {
   return (
-    <Center flexDir="column" minH="100vh" py={8}>
-      <Center
+    <Center flexDir="column" minH="100vh">
+      <Flex
         as="main"
         flexDir="column"
         flex="1"
-        paddingX={["1rem", "3rem", "5rem"]}
+        maxW="48rem"
+        maxH="max-content"
+        marginX={["1rem", "3rem", "5rem"]}
       >
-        <Logo boxSize={24} />
-        <Heading as="h1" size="2xl" mb={4} textAlign="center">
-          Next.js with Chakra UI
+        <Heading as="h1" fontSize={["5xl", "6xl"]} mb={4}>
+          Hello,
         </Heading>
-        <Heading as="h2" size="lg" mb={4} textAlign="center">
-          Starter Template
+        <Heading as="h2" fontSize={["5xl", "6xl"]} mb={4}>
+          I am Navin
         </Heading>
 
-        <Text fontSize="2xl" textAlign="center">
-          Get started by editing{" "}
-          <Code fontSize="xl" fontWeight="semibold" colorScheme="orange">
-            pages/index.js
-          </Code>
+        <Text fontSize={["2xl", "3xl"]}>
+          Teaching myself UI design and development while working as UI
+          Developer.
         </Text>
-
-        <Center
-          flexDirection={["column", null, "row"]}
-          flexWrap="wrap"
-          maxW="800px"
-          mt={12}
-        >
-          <Card title="Documentation &rarr;" link="https://nextjs.org/docs">
-            Find in-depth information about Next.js features and API.
-          </Card>
-          <Card title="Learn &rarr;" link="https://nextjs.org/learn">
-            Learn about Next.js in an interactive course with quizzes!
-          </Card>
-          <Card
-            title="Examples &rarr;"
-            link="https://github.com/vercel/next.js/tree/master/examples"
-          >
-            Discover and deploy boilerplate example Next.js projects.
-          </Card>
-          <Card
-            title="Deploy &rarr;"
-            link="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </Card>
-        </Center>
-      </Center>
+      </Flex>
     </Center>
-  )
-}
-
-function Card(props) {
-  const { title, link, children, ...rest } = props
-
-  return (
-    <Link
-      href={link}
-      flex="1 1 45%"
-      m={4}
-      p={6}
-      textDecoration="none"
-      borderWidth="1px"
-      borderRadius="10px"
-      transition="color 0.15s ease, border-color 0.15s ease"
-      _hover={{
-        textDecor: "none",
-        color: "teal.500",
-        borderColor: "teal.500",
-      }}
-      {...rest}
-    >
-      <Heading as="h3" size="lg" fontWeight="normal" mb={4}>
-        {`${title}`}
-      </Heading>
-      <Text fontSize="xl">{children}</Text>
-    </Link>
   )
 }
