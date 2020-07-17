e264c02d778423b1893b1a6b64fc058bfd0ff996
Navin <navin007.a@gmail.com>
2020-07-17 06:29:14 +0530

    refactor: :recycle: move head to _app from _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
-import { InitializeColorMode } from "@chakra-ui/color-mode"
 import GoogleFonts from "next-google-fonts"
+import { InitializeColorMode } from "@chakra-ui/color-mode"
 
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
@@ -12,68 +12,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
-        <Head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          <title>Navin Moorthy</title>
-          <meta name="title" content="Navin Moorthy" />
-          <meta name="description" content="Portfolio blog of Navin Moorthy" />
-
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <meta name="msapplication-config" content="/browserconfig.xml" />
-          <meta name="msapplication-TileColor" content="#EA2E6E" />
-          <meta name="theme-color" content="#EA2E6E" />
-
-          <title>Navin Moorthy</title>
-          <meta name="title" content="Navin Moorthy" />
-          <meta name="description" content="Portfolio blog of Navin Moorthy" />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="http://navinmoorthy.me/" />
-          <meta property="og:title" content="Navin Moorthy" />
-          <meta
-            property="og:description"
-            content="Portfolio blog of Navin Moorthy"
-          />
-          <meta
-            property="og:image"
-            content="http://navinmoorthy.me/og-image.jpg"
-          />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="http://navinmoorthy.me/" />
-          <meta property="twitter:title" content="Navin Moorthy" />
-          <meta
-            property="twitter:description"
-            content="Portfolio blog of Navin Moorthy"
-          />
-          <meta
-            property="twitter:image"
-            content="http://navinmoorthy.me/og-image.jpg"
-          ></meta>
-        </Head>
+        <Head />
         <body>
           <InitializeColorMode />
           <Main />
